18e27e9f101e3985386ddafa42de70f287816bc1
olly000 <ollystockman@gmail.com>
2019-07-17 17:04:09 +0100

    Threadsafe version - still need to re-grid and disable update button while ReMapMidi.direct_midi runs

--- MidiMapperV03.py
+++ MidiMapperV03.py
@@ -5,6 +5,7 @@
 import mido
 import pygame.midi as pym
 import tkinter as tk
+import threading
 from csv import reader
 from sys import exit
 
@@ -18,6 +19,7 @@ class Interface(tk.Frame):  # Creates a GUI frame for user interaction
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
+        self.run_state = True
         self.in_port = self.select_port(mido.get_input_names(), 'input')
         self.out_port = self.select_port(mido.get_output_names(), 'output')
         self.map_file = self.select_map_file()
@@ -31,9 +33,19 @@ class Interface(tk.Frame):  # Creates a GUI frame for user interaction
         print(data)
         return dict(zip(labels, data))
 
+    def end_app(self):
+        self.run_state = False
+        self.master.destroy()
+        print('App terminated by user')
+
+    def pause_loop(self):
+        self.run_state = False
+
     def start_processor(self):
+        self.run_state = True
         process = MidiReMap(self.grab_inputs())
-        process.direct_midi()
+        t = threading.Thread(target=process.direct_midi)
+        t.start()
 
     @staticmethod
     def select_port(port_list, direction):
@@ -63,25 +75,23 @@ class Interface(tk.Frame):  # Creates a GUI frame for user interaction
 
     def create_buttons(self):
         option_set = tk.Button(root, text='Update Settings', command=self.start_processor)
-        quit_button = tk.Button(root, text="Quit", bg="red", command=self.master.destroy)
+        pause = tk.Button(root, text='Pause', command=self.pause_loop)
+        quit_button = tk.Button(root, text="Quit", bg="red", command=self.end_app)
         option_set.grid(column=0, padx=30, pady=30)
+        pause.grid(column=1, padx=30, pady=10)
         quit_button.grid(column=1, padx=30, pady=30)
 
 
 class MidiReMap:  # class containing methods for processing MIDI according to user input
     def __init__(self, inputs):
-        self.in_port = mido.open_input(inputs['MIDI in'])
-        self.out_port = mido.open_output(inputs['MIDI out'])
-        self.channel = int(inputs['channel']) - 1
-        self.map = self.handle_map_file(inputs['map'])
+        self.inputs = inputs
+        self.in_port = mido.open_input(self.inputs['MIDI in'])
+        self.out_port = mido.open_output(self.inputs['MIDI out'])
+        self.channel = int(self.inputs['channel']) - 1
+        self.map = self.handle_map_file(self.inputs['map'])
 
-    def direct_midi(self):  # listens for MIDI input or key interruption
-        while True:
-            try:
-                self.port_listen()
-            except KeyboardInterrupt:
-                print('Terminated by User')
-                exit(0)
+    def open_out_port(self):
+        return mido.open_output(self.inputs['MIDI out'])
 
     @staticmethod
     def handle_map_file(file_name):  # asks user for cc map and creates a dictionary based on it
@@ -91,12 +101,28 @@ class MidiReMap:  # class containing methods for processing MIDI according to us
                 mappings[orig] = new
         return mappings
 
+    def handle_cc(self, msg):
+        if 'channel' in str(msg):
+            print(msg)
+            msg = self.check_cc(msg.copy(channel=self.channel))
+        self.out_port.send(msg)
+
+    def direct_midi(self):  # listens for MIDI input or key interruption
+        while True and app.run_state:
+            try:
+                self.port_listen()
+                self.in_port.close()
+                self.out_port.close()
+            except KeyboardInterrupt:
+                print('Terminated by User')
+                exit(0)
+
     def port_listen(self):  # takes message from input port and sends it to out after processing
         for msg in self.in_port:
-            if 'channel' in str(msg):
-                print(msg)
-                msg = self.check_cc(msg.copy(channel=self.channel))
-            self.out_port.send(msg)
+            if app.run_state:
+                self.handle_cc(msg)
+            else:
+                break
 
     def check_cc(self, msg):  # checks MIDI input - sends to transform if cc or output if not.
         msg = msg.bytes()
